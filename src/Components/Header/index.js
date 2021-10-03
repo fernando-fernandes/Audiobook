d011f24c9c978908186bd67d84f6d603d9f93b40
Fernando <fernando.85@outlook.com>
2021-10-03 18:01:06 -0300

    header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,17 +1,42 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
+import Flag from 'react-world-flags'
 
 export default function Header() {
     return (
         <div className="header-container">
             <div className="area-profile">
-                <div className="profile-container">
+                <div className="profile">
                     <FontAwesomeIcon icon={faUser} />
+                </div>
+                <div className="username">
+                    <h1>Nome do aluno</h1>
                 </div>
             </div>
 
             <div className="area-info">
-                texto
+
+                <h4 className="subjects">Disciplinas</h4> 
+                
+                <label className="flag">
+                    <input type="radio" className="flag-input" name="subjects" checked />
+                    <div className="flag-name">
+                        <div className="flag-name_img">
+                            <Flag code='US' className="left" />
+                        </div>
+                        <span className="flag-name_title">Inglês</span>
+                    </div>
+                </label>
+
+                <label className="flag">
+                    <input type="radio" className="flag-input" name="subjects" />
+                    <div className="flag-name">
+                        <div className="flag-name_img">
+                            <Flag code='JP' className="center" />
+                        </div>
+                        <span className="flag-name_title">Japonês</span>
+                    </div>
+                </label>
             </div>
         </div>
     )
